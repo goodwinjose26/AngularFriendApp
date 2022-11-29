b86b4f71ee9ce06b0f33f4c2cf32b3caafac0387
goodwinjose26 <josegoodwin22@gmail.com>
2022-11-29 12:21:49 +0530

    services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { SearchfriendComponent } from './searchfriend/searchfriend.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 const myRoute:Routes=[
   {
     path:"",
@@ -35,7 +36,8 @@ const myRoute:Routes=[
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot(myRoute),
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/searchfriend/searchfriend.component.ts
+++ src/app/searchfriend/searchfriend.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./searchfriend.component.css']
 })
 export class SearchfriendComponent {
+  name=""
+  
+
+  searchValues =()=>
+  {
+    let data:any={"name":this.name}
+console.log(data)
+  }
 
 }
